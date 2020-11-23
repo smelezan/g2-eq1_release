88626f6e8d34f19a1e05881a9f1b6a55ef084e4a
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2020-11-23 22:33:19 +0100

    Adding artefact tab in Project page

--- front/src/views/Project.vue
+++ front/src/views/Project.vue
@@ -3,8 +3,17 @@
 
         <v-main>
             <v-container>
-                <h4>{{$route.params.id}}</h4>
+                <h2 style="color:grey">{{$route.params.id}}</h2>
             </v-container>
+              <v-tabs>
+                <v-tab>Sprints</v-tab>
+                <v-tab>Issues</v-tab>
+                <v-tab>Tâches</v-tab>
+                <v-tab>Releases</v-tab>
+                <v-tab>Tests</v-tab>
+                <v-tab>Documentations</v-tab>
+            </v-tabs>
+
         </v-main>
     </v-app>
 </template>
@@ -15,6 +24,6 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
 </style>
