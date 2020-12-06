0f6c74ed40fd4298dea3cdfcebb2f48074410902
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2020-12-06 19:31:54 +0100

    Test runTest execition output

--- runTests.sh
+++ runTests.sh
@@ -3,18 +3,16 @@
 cd issues-back/
 npm install
 npm run-script test2
-cat test_2.json
+echo "coucou"
 #npm run-script build
 cd ../
 cd tasks-back/
 npm install
 npm run-script test2
-cat test_2.json
 #npm run-script build
 cd ../
 cd tests-back/
 npm install
 npm run-script test2
-cat test_2.json
 #npm run-script build
 cd ../
