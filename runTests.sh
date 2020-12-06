ff08e4656d8ba50b9ab4110139ed4e0f8929a062
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2020-12-06 19:21:57 +0100

    Cat test output in CI

--- runTests.sh
+++ runTests.sh
@@ -3,15 +3,18 @@
 cd issues-back/
 npm install
 npm run-script test2
+cat test_2.json
 #npm run-script build
 cd ../
 cd tasks-back/
 npm install
 npm run-script test2
+cat test_2.json
 #npm run-script build
 cd ../
 cd tests-back/
 npm install
 npm run-script test2
+cat test_2.json
 #npm run-script build
 cd ../
