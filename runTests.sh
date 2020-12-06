ae085614536cc95f52271e96e1aee554fe2fe004
smelezan <sebastien.melezan@etu.u-bordeaux.fr>
2020-12-06 20:58:01 +0100

    test

--- runTests.sh
+++ runTests.sh
@@ -2,17 +2,17 @@
 
 cd issues-back/
 npm install
-npm run-script test2
+# npm run-script test2
 echo "coucou"
 #npm run-script build
 cd ../
 cd tasks-back/
 npm install
-npm run-script test2
+# npm run-script test2
 #npm run-script build
 cd ../
 cd tests-back/
 npm install
-npm run-script test2
+# npm run-script test2
 #npm run-script build
 cd ../
